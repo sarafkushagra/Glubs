f525a16529b911f935b210915eb5728a42af10dd
sarafkushagra <ksaraf0004@gmail.com>
2025-07-25 11:39:36 +0530

    bug

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (options) => {
   try {
     const transporter = nodemailer.createTransport({
-      service: "Gmail",
+      service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
