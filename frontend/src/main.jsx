985d2280fa3a767d6369c481a90f9901d1afbd58
sarafkushagra <ksaraf0004@gmail.com>
2026-01-15 21:16:08 +0530

    clerk htao

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,17 +6,10 @@ import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './components/Context/userStore.jsx'
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { ClerkProvider } from '@clerk/clerk-react';
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error('Add your Clerk Publishable Key to the .env file')
-}
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
     <AuthProvider>
       <App />
 
@@ -29,6 +22,5 @@ createRoot(document.getElementById('root')).render(
       />
 
     </AuthProvider>
-    </ClerkProvider>
   </BrowserRouter>
 )
